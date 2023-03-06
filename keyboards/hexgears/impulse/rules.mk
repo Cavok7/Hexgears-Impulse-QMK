4d8a54126694bbb1543d9cd1d010fe1750b5ee2f
Cavok7 <jonathangilbert27@gmail.com>
2023-03-06 17:05:04 -0500

    v0

---
+++ keyboards/hexgears/impulse/rules.mk
@@ -0,0 +1,20 @@
+# MCU name
+MCU = SN32F248BF
+
+# Bootloader selection
+#BOOTLOADER = sn32-dfu
+
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite (Virtual DIP switch configuration)
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+WAIT_FOR_USB = no
